f1587e7576609d717f1d213cc8bb76946497ecf7
opmlib <info@osapropertymanagement.com>
2026-04-27 11:23:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,18 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch("toggle.html")
     .then(res => res.text())
     .then(html => {
-      const container = document.getElementById("toggle-container");
-      if (!container) return;
 
-      container.innerHTML = html;
+      document.getElementById("toggle-container").innerHTML = html;
 
-      // bind AFTER HTML is injected
       const toggle = document.getElementById("input");
       if (toggle) {
         toggle.addEventListener("change", () => {
           document.body.classList.toggle("dark", toggle.checked);
         });
       }
+
     });
 
 });
